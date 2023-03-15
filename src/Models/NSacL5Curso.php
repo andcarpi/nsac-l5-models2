32e24bc43456231bf7a8d8dafbb68a98b85edb03
andcarpi <andcarpi@users.noreply.github.com>
2023-03-15 15:01:12 +0000

    Fix styling

--- src/Models/NSacL5Curso.php
+++ src/Models/NSacL5Curso.php
@@ -5,9 +5,11 @@ namespace Andcarpi\NsacL5Models\Models;
 class NSacL5Curso extends BaseModel
 {
     protected $table = 'public.cursos';
+
     protected $primaryKey = 'codigo';
 
-    public function turmas() {
+    public function turmas()
+    {
         return $this->hasMany(NSacL5Turma::class, 'curso', $this->primaryKey);
     }
 }
--- src/Models/NSacL5Turma.php
+++ src/Models/NSacL5Turma.php
@@ -5,13 +5,16 @@ namespace Andcarpi\NsacL5Models\Models;
 class NSacL5Turma extends BaseModel
 {
     protected $table = 'public.turmas';
+
     protected $primaryKey = 'codigo';
 
-    public function curso() {
+    public function curso()
+    {
         return $this->belongsTo(NSacL5Curso::class, 'curso', 'codigo');
     }
 
-    public function disciplinas() {
-        return $this->hasMany(NSacL5Disciplina::class, 'turma', 'codigo')->where('ativo', TRUE);
+    public function disciplinas()
+    {
+        return $this->hasMany(NSacL5Disciplina::class, 'turma', 'codigo')->where('ativo', true);
     }
 }
