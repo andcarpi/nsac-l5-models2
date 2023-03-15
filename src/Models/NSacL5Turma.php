4f8a380d2e1b1ceb23226ecef50017e5a5b88ce9
andcarpi <kustelac@yahoo.com.br>
2023-03-15 12:02:06 -0300

    - Relacionamento dos Models Curso e Disciplina

--- src/Models/NSacL5Turma.php
+++ src/Models/NSacL5Turma.php
@@ -5,4 +5,13 @@ namespace Andcarpi\NsacL5Models\Models;
 class NSacL5Turma extends BaseModel
 {
     protected $table = 'public.turmas';
+    protected $primaryKey = 'codigo';
+
+    public function curso() {
+        return $this->belongsTo(NSacL5Curso::class, 'curso', 'codigo');
+    }
+
+    public function disciplinas() {
+        return $this->hasMany(NSacL5Disciplina::class, 'turma', 'codigo')->where('ativo', TRUE);
+    }
 }
